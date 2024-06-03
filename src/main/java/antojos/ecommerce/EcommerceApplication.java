577c7d45df47044fe80bc25027912468832e76a0
LucianoArmas <lucianoarmas11@gmail.com>
2024-06-02 21:08:42 -0300

    Modifying something

--- src/main/java/antojos/ecommerce/EcommerceApplication.java
+++ src/main/java/antojos/ecommerce/EcommerceApplication.java
@@ -2,11 +2,13 @@ package antojos.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "antojos.ecommerce.repositories")
 public class EcommerceApplication {
 
 
--- src/main/java/antojos/ecommerce/config/ApplicationConfig.java
+++ src/main/java/antojos/ecommerce/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import antojos.ecommerce.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableJpaRepositories(basePackages = "antojos.ecommerce.repositories")
 public class ApplicationConfig {
   private final UserRepository userRepository;
 
